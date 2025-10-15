2774681e35d820af1e877285bbedff50cfa91f78
Edison Ndou <ndouedison02@gmail.com>
2025-10-15 15:51:16 +0200

    Adding Profile

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,3 +1,4 @@
+import { Ionicons } from '@expo/vector-icons';
 import { Stack, Tabs } from 'expo-router';
 import React from 'react';
 import { View, Text, StyleSheet, useColorScheme, StatusBar } from 'react-native';
@@ -7,7 +8,22 @@ const RootLayout = () => {
     return (
       <SafeAreaView style={{ flex: 1}} edges={['top']}>
         <Tabs screenOptions={{ headerShown: false }}>
-          <Tabs.Screen name="homepage" options={{ title: 'Homepage'}} />
+          <Tabs.Screen name="homepage" options={{ title: 'Homepage', tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'home' : 'home-outline'}
+              size={24}
+              color={focused ? '#a90000ff' : '#999'}
+            />
+            ),
+          }} />
+          <Tabs.Screen name="myprofile" options={{ title: 'My Profile', tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons
+              name={focused ? 'home' : 'home-outline'}
+              size={24}
+              color={focused ? '#a90000ff' : '#999'}
+            />
+            ),
+          }} />
           <Tabs.Screen name="index" options={{ title: 'Index', tabBarButton: () => null, tabBarStyle: { display: 'none' } }} />
           <Tabs.Screen name="onboard" options={{ title: 'Onboard', tabBarButton: () => null, tabBarStyle: { display: 'none' } }} />
           <Tabs.Screen name="secondOnBoard" options={{ title: 'Second Onboard', tabBarButton: () => null, tabBarStyle: { display: 'none' } }} />
