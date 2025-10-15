7498b93ce94006b17975798e5ad3fa5cd34704c5
Edison Ndou <ndouedison02@gmail.com>
2025-10-15 15:43:31 +0200

    Fix

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -5,13 +5,15 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 const RootLayout = () => {
     return (
-      <Tabs screenOptions={{ headerShown: true }}>
-        <Tabs.Screen name="homepage" options={{ title: 'Homepage'}} />
-        <Tabs.Screen name="index" options={{ title: 'Index', tabBarButton: () => null, tabBarStyle: { display: 'none' } }} />
-        <Tabs.Screen name="onboard" options={{ title: 'Onboard', tabBarButton: () => null, tabBarStyle: { display: 'none' } }} />
-        <Tabs.Screen name="secondOnBoard" options={{ title: 'Second Onboard', tabBarButton: () => null, tabBarStyle: { display: 'none' } }} />
-        <Tabs.Screen name="start" options={{ title: 'Start', tabBarButton: () => null, tabBarStyle: { display: 'none' } }} />
-      </Tabs>
+      <SafeAreaView style={{ flex: 1}} edges={['top']}>
+        <Tabs screenOptions={{ headerShown: false }}>
+          <Tabs.Screen name="homepage" options={{ title: 'Homepage'}} />
+          <Tabs.Screen name="index" options={{ title: 'Index', tabBarButton: () => null, tabBarStyle: { display: 'none' } }} />
+          <Tabs.Screen name="onboard" options={{ title: 'Onboard', tabBarButton: () => null, tabBarStyle: { display: 'none' } }} />
+          <Tabs.Screen name="secondOnBoard" options={{ title: 'Second Onboard', tabBarButton: () => null, tabBarStyle: { display: 'none' } }} />
+          <Tabs.Screen name="start" options={{ title: 'Start', tabBarButton: () => null, tabBarStyle: { display: 'none' } }} />
+        </Tabs>
+      </SafeAreaView>
   );
 };
 
