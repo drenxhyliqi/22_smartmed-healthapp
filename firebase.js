b5d348bff2e720bfe0ce98d974d238deb6ae5952
Edison Ndou <ndouedison02@gmail.com>
2025-11-14 13:32:01 +0100

    Adding database

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBHbX3KeqgLBgU-b0pVk7AAYmvL5727308",
+  authDomain: "healthcareapp-v2025.firebaseapp.com",
+  projectId: "healthcareapp-v2025",
+  storageBucket: "healthcareapp-v2025.firebasestorage.app",
+  messagingSenderId: "247278658002",
+  appId: "1:247278658002:web:33b77c8469c1edd9bf280b",
+  measurementId: "G-F3X58J483F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
